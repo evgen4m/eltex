9428ae5f8d0e8a1cec49235a821eed32c2d0f9cd
Shutikov Evgeni <shutikov.e@yandex.ru>
2022-01-24 18:08:10 +0700

    Небольшие исправления. Убрал неиспользуемые методы в TokenModel.java

--- app/src/main/java/com/esoft/eltex/domain/TokenModel.java
+++ app/src/main/java/com/esoft/eltex/domain/TokenModel.java
@@ -21,39 +21,7 @@ public class TokenModel {
 
     public String getToken() { return token; }
 
-    public void setToken(String token) {
-        this.token = token;
-    }
-
     public String getTokenType() {
         return tokenType;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
-
-    public Long getExpiresIn() {
-        return expiresIn;
-    }
-
-    public void setExpiresIn(Long expiresIn) {
-        this.expiresIn = expiresIn;
-    }
-
-    public String getScope() {
-        return scope;
-    }
-
-    public void setScope(String scope) {
-        this.scope = scope;
     }
 }
